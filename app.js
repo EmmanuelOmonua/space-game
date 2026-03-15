eaaa1fb657a2e5cec7753c59cbb6158d9e595c0e
EmmanuelOmonua <omonua@uwindsor.ca>
2026-03-15 00:44:47 +0000

    Added canvas rendering

--- app.js
+++ app.js
@@ -1,82 +1,10 @@
-const canvas = document.getElementById("gameCanvas");
-const ctx = canvas.getContext("2d");
+let canvas;
+let ctx;
 
-canvas.width = 800;
-canvas.height = 600;
+window.onload = () => {
+  canvas = document.getElementById("canvas");
+  ctx = canvas.getContext("2d");
 
-let player = {
-    x: 375,
-    y: 520,
-    width: 50,
-    height: 50,
-    speed: 6
+  ctx.fillStyle = "black";
+  ctx.fillRect(0,0,canvas.width,canvas.height);
 };
-
-let bullets = [];
-let enemies = [];
-
-document.addEventListener("keydown", keyDown);
-
-function keyDown(e){
-
-    if(e.key === "ArrowLeft")
-        player.x -= player.speed;
-
-    if(e.key === "ArrowRight")
-        player.x += player.speed;
-
-    if(e.key === " ")
-        bullets.push({
-            x: player.x + 20,
-            y: player.y
-        });
-}
-
-function createEnemy(){
-
-    enemies.push({
-        x: Math.random() * 750,
-        y: 0,
-        width: 40,
-        height: 40
-    });
-}
-
-setInterval(createEnemy,1500);
-
-function update(){
-
-    bullets.forEach(b => b.y -= 10);
-
-    enemies.forEach(e => e.y += 2);
-}
-
-function draw(){
-
-    ctx.clearRect(0,0,800,600);
-
-    ctx.fillStyle = "green";
-    ctx.fillRect(player.x,player.y,player.width,player.height);
-
-    ctx.fillStyle = "red";
-
-    enemies.forEach(e=>{
-        ctx.fillRect(e.x,e.y,e.width,e.height);
-    });
-
-    ctx.fillStyle = "yellow";
-
-    bullets.forEach(b=>{
-        ctx.fillRect(b.x,b.y,5,10);
-    });
-}
-
-function gameLoop(){
-
-    update();
-    draw();
-
-    requestAnimationFrame(gameLoop);
-}
-
-gameLoop();
