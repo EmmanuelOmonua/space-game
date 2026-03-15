a2c9b08d3456cd1aae7905a4bdfae3fecce15e6b
EmmanuelOmonua <omonua@uwindsor.ca>
2026-03-15 01:16:08 +0000

    Added collision detection

--- app.js
+++ app.js
@@ -124,3 +124,9 @@ window.addEventListener("keyup",(evt)=>{
   }
 });
 
+function intersectRect(r1,r2){
+  return !(r2.left > r1.right ||
+           r2.right < r1.left ||
+           r2.top > r1.bottom ||
+           r2.bottom < r1.top);
+}
