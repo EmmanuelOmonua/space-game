a806e2d1a776bde11ccf548303289d0255416765
EmmanuelOmonua <omonua@uwindsor.ca>
2026-03-15 00:45:26 +0000

    Added GameObject base class

--- app.js
+++ app.js
@@ -8,3 +8,28 @@ window.onload = () => {
   ctx.fillStyle = "black";
   ctx.fillRect(0,0,canvas.width,canvas.height);
 };
+
+class GameObject {
+  constructor(x,y){
+    this.x = x;
+    this.y = y;
+    this.dead = false;
+    this.type = "";
+    this.width = 0;
+    this.height = 0;
+    this.img = undefined;
+  }
+
+  draw(ctx){
+    ctx.drawImage(this.img,this.x,this.y,this.width,this.height);
+  }
+
+  rectFromGameObject(){
+    return {
+      top: this.y,
+      left: this.x,
+      bottom: this.y + this.height,
+      right: this.x + this.width
+    };
+  }
+}
